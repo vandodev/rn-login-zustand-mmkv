c41e0b0ad3b61a68b37084d9c8a5b6f05601d70d
vandodev <vandoaparecido@hotmail.com>
2025-10-02 07:21:40 -0300

    :zap::lock::key: Limpando projeto

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+// app/_layout.tsx
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native"
+import { Stack } from "expo-router"
+import { StatusBar } from "expo-status-bar"
+import { useColorScheme } from "react-native"
+import "react-native-reanimated"
 
 export const unstable_settings = {
-  anchor: '(tabs)',
-};
+  initialRouteName: "(tabs)", // rota inicial
+}
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme() // detecta claro/escuro
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+        <Stack.Screen
+          name="index"
+          options={{ presentation: "modal", title: "Modal", headerShown:false}}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
-  );
+  )
 }
