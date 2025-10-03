0e2b0096040226903b9502b3e15d363df0049a25
vandodev <vandoaparecido@hotmail.com>
2025-10-03 07:45:56 -0300

    :zap: :lock: :key:  Criando campos e estilizando tela de login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  const isAltenticated = true
+  const isAltenticated = false
   return (  
     <Stack>
 
--- app/login.tsx
+++ app/login.tsx
@@ -1,20 +1,35 @@
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
 
 const LoginScreen = () => {
+    const [userName, setUsername] = useState("")
+    const [passWord, setPassWord] =  useState("")
     return(
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Tela de login</Text>
+        <View style={styles.container}>     
+
+            <Text>Nome de usuário</Text>
+            <TextInput value={userName} placeholder="Nome de osoário" style={styles.input}/>
+
+            <Text>Senha</Text>
+            <TextInput value={passWord} placeholder="Senha" secureTextEntry style={styles.input}/>
+
+            <Button title="Entrar"  onPress={() => {}}/>
+        
         </View>
     )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal:20
+    },
+    input:{
+        borderWidth: 1,
+        marginBottom: 10,
+        padding: 10
+    }
+})
