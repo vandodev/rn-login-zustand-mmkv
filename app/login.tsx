e0133503e618d2248db9b12588421c322cf20e70
vandodev <vandoaparecido@hotmail.com>
2025-10-05 07:23:57 -0300

    :zap: :lock: :key: Refresh token

--- app/login.tsx
+++ app/login.tsx
@@ -24,6 +24,7 @@ const LoginScreen = () => {
                 onChangeText={setUsername} 
                 placeholder="Nome de osoário" 
                 style={styles.input}
+                autoCapitalize="none"
              />
 
             <Text>Senha</Text>
--- store/authStore.ts
+++ store/authStore.ts
@@ -8,6 +8,7 @@ interface AuthStore {
     user: object | null
     accessTokenExpiration: number | null
     Login: (username: string, password: string) => Promise<boolean>
+    refreshAccessToken: () => Promise<boolean>
     Logout: () => void
     checkAuth: () => void
 }
@@ -66,11 +67,58 @@ export const useAuthStore = create<AuthStore>((set, get) => ({
         })
     },
 
+    refreshAccessToken: async () =>{
+        try{
+            const {refreshToken} = get();
+            const res = await  fetch('https://dummyjson.com/auth/refresh',{
+                method: "POST",
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({refreshToken}),
+                credentials: 'include'
+            });
+            
+            if(!res.ok){
+                throw new Error("Invalid credentials")
+            }
+
+            const data = await res.json();
+            const updateAuthData = {
+                ...get(),
+                accessToken: data.accessToken,
+                refreshToken:data.refreshToken,
+                accessTokenExpiration: Date.now() * 60 * 60 * 1000,
+            };
+
+            set(updateAuthData);
+            setItem('authData', updateAuthData)
+            return true
+        } catch(error){
+            console.error('Error refreshing access token', error);
+            get().Logout();
+            return false
+        }
+    },
+   
+
+
     checkAuth: async () =>  {
         const authData = getItem('authData');
-        if(authData){
+        if(!authData){            
+            return false
+        }
+
+        const {accessToken, accessTokenExpiration, refreshToken} = authData
+       
+        if(accessToken && accessTokenExpiration && accessTokenExpiration > Date.now()){
             set(authData)
             return true
+        }
+
+        if(refreshToken){
+            set(authData)
+            return await get().refreshAccessToken();
         }
 
         return false
